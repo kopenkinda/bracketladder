32769928e35e6f4bc7f57baba19f5c0d81be9aca
Dmitrii <kopenkin.da@gmail.com>
2022-12-09 11:56:54 +0100

    fix wrong prisma var in user router

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -2,12 +2,14 @@ import { z } from 'zod';
 import { protectedProcedure, router } from '../trpc';
 
 export const userRouter = router({
-	getTournaments: protectedProcedure
-		.input(z.string({ description: 'User ID' }))
-		.query(({ input }) => {
-		return prisma?.user.findUnique({
-			where: { id: input },
-			include: { particiaptesInTournaments: true },
-		}).then((res) => res?.particiaptesInTournaments || []);
-	})
-})
+  getTournaments: protectedProcedure
+    .input(z.string({ description: 'User ID' }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.user
+        .findUnique({
+          where: { id: input },
+          include: { particiaptesInTournaments: true },
+        })
+        .then((res) => res?.particiaptesInTournaments || []);
+    }),
+});
